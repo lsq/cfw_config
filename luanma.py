2bdbddb2f300abb1bd96d5a484bc0c32b7c692ae
lsq <lsqypj@gmail.com>
2025-12-24 18:30:46 +0800

    add luanma test

--- luanma.py
+++ luanma.py
@@ -8,3 +8,9 @@ print(f'byte(latin1): {original_bytes}')
 # 再用 utf-8 解码
 correct_text = original_bytes.decode('utf-8')  # '节点'
 print(f'解码后：{correct_text}')
+
+s = '鍧€'
+original_bytes = s.encode('gbk')  # b'\xe8\x8a\x82\xe7\x82\xb9'
+print(f'byte(latin1): {original_bytes}')
+correct_text = original_bytes.decode('utf-8')  # '节点'
+print(f'解码后：{correct_text}')
